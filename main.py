8df98b124a9f40cef35be6fc310a98d48e002be9
JoshLolo <110125566+JoshLolo@users.noreply.github.com>
2022-08-13 04:34:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import Course_Project
+
+while True:
+ city = (input("Please enter a zipcode or city (n to quit): "))  
+ print()
+ if city == 'n':
+   print("Goodbye")
+   break
+ else:
+   Course_Project.notmain(city)
+   print()
